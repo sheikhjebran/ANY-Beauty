cd9f44a894313daa83c3c6174dee7402ca89eb98
AynBeauty <aynbeautyweb@gmail.com>
2025-08-12 15:05:20 +0000

    can u make sure the entire project is configure to use firebase as datab

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPfXrtEuV0L5V-WNiozHjAjRVWDzgJEa4",
+  authDomain: "ayn-beauty.firebaseapp.com",
+  projectId: "ayn-beauty",
+  storageBucket: "ayn-beauty.appspot.com",
+  messagingSenderId: "557166650929",
+  appId: "1:557166650929:web:55e600cfb963d91edc1710"
+};
+
+// Initialize Firebase for client-side usage
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
