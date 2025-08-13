880c4ae5bfb4ca7b87d072d67dc4662249407868
Sheikh Jebran <sheikhjebran@gmail.com>
2025-08-14 00:28:30 +0530

    firebase credentials

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDPfXrtEuV0L5V-WNiozHjAjRVWDzgJEa4",
+  authDomain: "ayn-beauty.firebaseapp.com",
+  projectId: "ayn-beauty",
+  storageBucket: "ayn-beauty.appspot.com",
+  messagingSenderId: "557166650929",
+  appId: "1:557166650929:web:55e600cfb963d91edc1710"
 };
 
 // Initialize Firebase for client-side usage
